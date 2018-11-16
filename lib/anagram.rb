fd7daecfbb5e93e8c48d534476e92c3194620229
Devin Sweeting <devinsweeting@gmail.com>
2018-11-16 13:20:24 -0800

    refactor to include class Anagram and pass same tests

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,30 +1,24 @@
-# class Word
-#   attr_writer(:word,)
-#
-#   def initialize(word1, word2)
-#     @word1 = word1
-#     @word2 = word2
-#   end
+class Anagram
+  attr_writer(:word)
 
-  def count_vowels(string)
-    count = string.chars.count {|char| "aeiou".include?(char)}
-    puts count
-    return count
+  def initialize(word)
+    @word = word
   end
 
-  def anagram_check(word1, word2)
-    count1 = word1.chars.count {|char| "aeiou".include?(char)}
-    count2 = word2.chars.count {|char| "aeiou".include?(char)}
-    ana = word1.downcase().split('').sort()
-    gram = word2.downcase().split('').sort()
+
+  def anagram_check(input)
+    count1 = @word.chars.count {|char| "aeiou".include?(char)}
+    count2 = input.chars.count {|char| "aeiou".include?(char)}
+    ana = @word.downcase().split('').sort()
+    gram = input.downcase().split('').sort()
     if ((count1 == 0) || (count2 == 0))
-      puts "Not even a word!"
+      puts "#{input} is not even a word!"
       return false
     elsif (ana.each.any?{|char| gram.include?(char)}) == false
       puts "This is an antigram!"
       return false
     elsif (ana == gram)
-      puts "This is an anagram!"
+      puts "#{input} is an anagram to #{@word}"
       return true
     else
       puts "This is NOT an anagram!"
@@ -32,10 +26,46 @@
     end
   end
 
+  # def anagram_check(word1, word2)
+  #   count1 = word1.chars.count {|char| "aeiou".include?(char)}
+  #   count2 = word2.chars.count {|char| "aeiou".include?(char)}
+  #   ana = word1.downcase().split('').sort()
+  #   gram = word2.downcase().split('').sort()
+  #   if ((count1 == 0) || (count2 == 0))
+  #     puts "Not even a word!"
+  #     return false
+  #   elsif (ana.each.any?{|char| gram.include?(char)}) == false
+  #     puts "This is an antigram!"
+  #     return false
+  #   elsif (ana == gram)
+  #     puts "This is an anagram!"
+  #     return true
+  #   else
+  #     puts "This is NOT an anagram!"
+  #     return false
+  #   end
+  # end
+
+  # def count_vowels(@word)
+  #   count = string.chars.count {|char| "aeiou".include?(char)}
+  #   puts count
+  #   return count
+  # end
+
+end
+
+
+# -------------- Notes and extra methods ----------------
+
+# ana = ['a','s','s']
+# gram = ['b','a','s','s']
+# anti = ['f','l','y']
+# grams = ['b','a','d']
+# anti.each.any?{|char| grams.include?(char)}
+# ana.each.any?{|char| gram.include?(char)}
+#
+# def count_vowels(string)
+#   count = string.chars.count {|char| "aeiou".include?(char)}
+#   puts count
+#   return count
 # end
-ana = ['a','s','s']
-gram = ['b','a','s','s']
-anti = ['f','l','y']
-grams = ['b','a','d']
-anti.each.any?{|char| grams.include?(char)}
-ana.each.any?{|char| gram.include?(char)}
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -1,31 +1,37 @@
-require('anagram')
+require('Anagram')
 require ('pry')
 require ('rspec')
 
-describe('anagram') do
+describe('Anagram#anagram_check') do
 # anagram method
   it("Expect 'hello' to match 'hello'") do
-    expect(anagram_check('hello', 'hello')).to(eq(true))
+    word = Anagram.new('hello')
+    expect(word.anagram_check('hello')).to(eq(true))
   end
 
   it("Expect 'hi' to not match 'bye'") do
-    expect(anagram_check('hi', 'bye')).to(eq(false))
+    word = Anagram.new('hi')
+    expect(word.anagram_check('bye')).to(eq(false))
   end
 
   it("Expect 'HEllo' to match 'heLLO'") do
-    expect(anagram_check('HEllo', 'heLLO')).to(eq(true))
+    word = Anagram.new('HEllo')
+    expect(word.anagram_check('heLLO')).to(eq(true))
   end
 
   it("Expect 'trn' to not match 'wtm'") do
-    expect(anagram_check('trn', 'wtm')).to(eq(false))
+    word = Anagram.new('trn')
+    expect(word.anagram_check('wtm')).to(eq(false))
   end
 
   it("Expect 'hi' to be an antigram of 'bye'") do
-    expect(anagram_check('hi', 'bye')).to(eq(false))
+    word = Anagram.new('hi')
+    expect(word.anagram_check('bye')).to(eq(false))
   end
 
+
 # is a vowel method
-  it("Expect 'hello' to return 2 vowels") do
-    expect(count_vowels('hello')).to(eq(2))
-  end
+  # it("Expect 'hello' to return 2 vowels") do
+  #   expect(count_vowels('hello')).to(eq(2))
+  # end
 end
