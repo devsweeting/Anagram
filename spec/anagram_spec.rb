743b69ed163468bcf0efe00ec74b64821e67e6c1
Devin Sweeting <devinsweeting@gmail.com>
2018-11-16 11:08:28 -0800

    write second test to account for words with different cases

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -7,4 +7,8 @@ describe('anagram') do
   it("Expect 'hello' to match 'hello'") do
     expect(anagram_check('hello', 'hello')).to(eq(true))
   end
+
+  it("Expect 'HEllo' to match 'heLLO'") do
+    expect(anagram_check('HEllo', 'heLLO')).to(eq(true))
+  end
 end
